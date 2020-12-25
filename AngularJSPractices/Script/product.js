053e54a9608195537ba35f837032097bd54895a9
dhara.bhuva <dhara.bhuva@radixweb.com>
2020-12-25 10:14:16 +0530

    #191188 - AngulasJS practices task completed

---
+++ AngularJSPractices/Script/product.js
@@ -0,0 +1,23 @@
+﻿
+app.controller('ProductController', function ($scope) {
+    $scope.ProdName = "";
+    $scope.ProdPrice = "";
+    $scope.ProdQuantity = "";
+    $scope.ProdSaleValue = "";
+
+    $scope.CalculateSaleValue = function () {
+        if ($scope.ProdPrice > 0 && $scope.ProdQuantity > 0) {
+            $scope.ProdSaleValue = $scope.ProdPrice * $scope.ProdQuantity;
+            return $scope.ProdSaleValue
+        }
+        else {
+            return 0;
+        }
+    };
+    $scope.Reset = function () {
+        $scope.ProdName = "";
+        $scope.ProdPrice = "";
+        $scope.ProdQuantity = "";
+        $scope.ProdSaleValue = "";
+    };
+});
