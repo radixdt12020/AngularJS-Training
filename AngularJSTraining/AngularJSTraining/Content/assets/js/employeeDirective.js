784785d205d05d1ddc20b895563e9997adb0631f
tarjani.pandya <tarjani.pandya@radixweb.com>
2020-12-29 17:50:59 +0530

    #191187 - Practice Exercise and Assignment : Done - Refer Folder Session1 #191658 - Practice Exercise  : Done, Assignment: In Progress - Refer Folder Session2

---
+++ AngularJSTraining/AngularJSTraining/Content/assets/js/employeeDirective.js
@@ -0,0 +1,28 @@
+app.directive('employeeDirective', function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, element, attr, mCtrl) {
+            function myValidation(value) {
+                if (sessionStorage.getItem("employeeList")) {
+                    var employeeList = JSON.parse(sessionStorage.getItem("employeeList"));
+                    var getEmp = employeeList.filter(function (filterEle) {
+                        if (filterEle.empCode == value) {
+                            return filterEle
+                        }
+                    })
+                    if (getEmp && getEmp.length > 0) {
+                        mCtrl.$setValidity('isExistEmpCode', false);
+                    }
+                    else {
+                        mCtrl.$setValidity('isExistEmpCode', true);
+                    }
+                }
+                else {
+                    mCtrl.$setValidity('isExistEmpCode', true);
+                }
+                return value;
+            }
+            mCtrl.$parsers.push(myValidation);
+        }
+    };
+});
