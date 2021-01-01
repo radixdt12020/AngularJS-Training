26c00052bc990ec34dd82a0fef03c79cfd1095d5
dhara.bhuva <dhara.bhuva@radixweb.com>
2021-01-01 16:14:43 +0530

    # 191659 - Product Inventory System Assignment is completed - Login credential: Username: admin & Password: admin

--- AngularJSSession2/AngularJSSession2/Session2Assignment/main.js
+++ AngularJSSession2/AngularJSSession2/Session2Assignment/main.js
@@ -4,6 +4,7 @@ app.config(function ($routeProvider) {
     $routeProvider
         .when("/", {
             template: "<div>Welcome to Product Inventory Application...!</div>",
+            //templateUrl:"Index.html",
             controller: "mainController"
         })
         .when("/productList", {
@@ -21,33 +22,69 @@ app.config(function ($routeProvider) {
         .when("/productDelete/:ProdId", {
             templateUrl: "Product/productDelete.html",
             controller: "productController"
-        })
-        .when("/login", {
-            templateUrl: "Login.html",
-            controller: "loginController"
         })        
 });
 
-app.controller("mainController", function ($scope, $window, $location) {
+app.controller("mainController", function ($scope, $window, $location, ProductService) {
 
     $scope.IsLogin = false;
     var id = $window.sessionStorage.getItem("IsLogin");
 
     if (id) {
-        $scope.IsLogin = true;        
+        $scope.IsLogin = true;
     } else {
-        $scope.IsLogin = false;        
+        $scope.IsLogin = false;
     }
-    $location.path("/");
-
+    
     $scope.Logout = function () {
         var check = confirm("Are you sure want to logout?");
         if (check) {
             $window.sessionStorage.removeItem("IsLogin");
+            $window.sessionStorage.removeItem("User");
             $scope.IsLogin = false;
             $location.url("/");
-            //$window.location.reload();
         }
     }
+
+
+    ////login control code   
+    $scope.ObjLogin = {
+        UserName: "",
+        Password: ""
+    }
+    $scope.showMsg = false;
+    $scope.message = "";
+
+    $scope.Login = function (objLogin) {
+
+        var userExist = ProductService.CheckUserExist(objLogin);
+        userExist.then(function (response) {
+            if (response.data.isSuccess) {
+                //alert("User exist!");
+                $window.sessionStorage.setItem("IsLogin", true);
+                $window.sessionStorage.setItem("User", response.data.json.UserName);
+                $scope.IsLogin = true;
+
+                //alert("You are successfully logged in..!")
+                $location.url("/");
+
+            } else {
+                //alert("Something went wrong!");
+                $scope.showMsg = true;
+                $scope.message = response.data.message;
+            }
+        }, function (error) {
+            $scope.showMsg = true;
+            $scope.message = "Something went wrong!";
+            console.log("Error: " + error);
+        });
+
+    };
+
+    $scope.Reset = function () {
+        $scope.showMsg = false;
+        $scope.ObjLogin = {};
+        $scope.loginForm.$setUntouched();
+    };
 
 });
